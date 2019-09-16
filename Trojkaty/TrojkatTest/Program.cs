4488ccaf8d191593a03d39edcdb39ec144516d29
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-16 20:28:47 +0200

    wer2

--- Trojkaty/TrojkatTest/Program.cs
+++ Trojkaty/TrojkatTest/Program.cs
@@ -11,10 +11,16 @@ namespace TrojkatTest
         static void Main(string[] args)
         {
             Trojkat x;
-            x = new Trojkat(2, 1, 3.2);
+            x = new Trojkat(2, 1, 2.5);
             Console.WriteLine(x);
             Console.WriteLine( x.Obwod() ); // funkcja Obwód
             Console.WriteLine( x.Obwód );   // properties get Obwod
+            //x.C = 4;
+            Console.WriteLine(x.Obwód);
+            Console.WriteLine(x.Pole);
+
+            var y = new Trojkat();
+            Console.WriteLine(y);
         }
     }
 }
