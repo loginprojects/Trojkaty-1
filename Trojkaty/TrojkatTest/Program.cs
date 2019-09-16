7478de1f99892baf425296b52f0e5f9a968e5426
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-16 18:46:26 +0200

    wer. 1

---
+++ Trojkaty/TrojkatTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrojkatTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat x;
+            x = new Trojkat(2, 1, 3.2);
+            Console.WriteLine(x);
+            Console.WriteLine( x.Obwod() ); // funkcja Obwód
+            Console.WriteLine( x.Obwód );   // properties get Obwod
+        }
+    }
+}
---
+++ Trojkaty/TrojkatTest/Trojkat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrojkatTest
+{
+    class Trojkat
+    {
+        // fields
+        private decimal a;
+        private decimal b;
+        private decimal c;
+
+        // kostruktory
+        public Trojkat(double a, double b, double c )
+        {
+            this.a = (decimal)a;
+            this.b = (decimal)b;
+            this.c = (decimal)c;
+        }
+
+        // przeciążenie ToString
+        public override string ToString()
+        {
+            return $"Trojkat: a={a} b={b} c={c}";
+        }
+
+        public double Obwod()
+        {
+            return (double)(a + b + c);
+        }
+
+        public double Obwód
+        {
+            get { return (double)(a + b + c); }
+        }
+    }
+}
